c95efbbb48172579c7fe466c56d12ecb2844dcc3
3e08488f9bcc2dd50192b3db89f6612c <69107484+3e08488f9bcc2dd50192b3db89f6612c@users.noreply.github.com>
2022-04-01 05:54:32 -0300

    Add files via upload

---
+++ update/src/BindableBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Update
+{
+	public abstract class BindableBase : INotifyPropertyChanged
+	{
+		// Member Variables
+		public event PropertyChangedEventHandler PropertyChanged;
+		// Member Functions
+		protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+		{
+			if(object.Equals(storage, value)) return false;
+			storage = value;
+			this.OnPropertyChanged(propertyName);
+			return true;
+		}
+		protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+		{
+			PropertyChangedEventHandler propertyChanged = this.PropertyChanged;
+			if(propertyChanged == null) return;
+			propertyChanged(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+}
---
+++ update/src/UpdateState.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Update
+{
+	public enum UpdateState
+	{
+		CheckingForTransformiceExe,
+		Downloading, // Download the executable from server
+		Updating // Extract and replace it.
+	}
+}
