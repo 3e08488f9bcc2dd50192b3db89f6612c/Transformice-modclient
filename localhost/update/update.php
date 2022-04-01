38b3491c14c9115886b8ce2541ec2fdebc5197dd
3e08488f9bcc2dd50192b3db89f6612c <69107484+3e08488f9bcc2dd50192b3db89f6612c@users.noreply.github.com>
2022-04-01 06:31:33 -0300

    Add files via upload

---
+++ localhost/update/update.php
@@ -0,0 +1,15 @@
+<?php
+$filename = "/download/update.zip";
+
+header('Content-Description: File Transfer');
+header('Content-Type: application/octet-stream');
+header("Cache-Control: no-cache, must-revalidate");
+header("Expires: 0");
+header('Content-Disposition: attachment; filename="'.basename($filename).'"');
+header('Content-Length: ' . filesize($filename));
+header('Pragma: public');
+flush();
+readfile($filename);
+die();
+
+?>
