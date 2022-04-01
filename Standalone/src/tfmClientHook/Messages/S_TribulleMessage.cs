04ed56b9997b14f5d86c4275851b2f384ad66397
3e08488f9bcc2dd50192b3db89f6612c <69107484+3e08488f9bcc2dd50192b3db89f6612c@users.noreply.github.com>
2022-04-01 08:19:10 -0300

    Add files via upload

---
+++ Standalone/src/tfmClientHook/Messages/S_TribulleMessage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace tfmClientHook.Messages
+{
+	public abstract class S_TribulleMessage : S_Message
+	{
+		public override ByteBuffer GetBuffer()
+		{
+			return null;
+		}
+		public abstract short TribulleId { get; }
+	}
+}
