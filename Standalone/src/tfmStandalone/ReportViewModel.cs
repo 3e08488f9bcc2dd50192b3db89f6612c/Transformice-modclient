f8dfff80a0c5015972e40d5d9691a2b2f678135b
3e08488f9bcc2dd50192b3db89f6612c <69107484+3e08488f9bcc2dd50192b3db89f6612c@users.noreply.github.com>
2022-04-06 12:55:23 -0300

    Add files via upload

---
+++ Standalone/src/tfmStandalone/ReportViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace tfmStandalone
+{
+	public sealed class ReportViewModel
+	{
+		public string ReporterName { get; set; }
+		public short ReporterKarma { get; set; }
+		public string Comment { get; set; }
+		public string Type { get; set; }
+		public short Age { get; set; }
+	}
+}
